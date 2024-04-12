94267dcd1c2156af4dafc67934409bb26027c1ed
Faraji, Afa <afaraji@loc.gov>
2024-04-12 13:30:40 -0400

    updated folder

--- src/test/java/runners/UITestRunner.java
+++ src/test/java/runners/UITestRunner.java
@@ -8,7 +8,7 @@ import org.junit.runner.RunWith;
                 "json:target/cucumber.json"},
         features="src/test/resources/uiFeatures",
         glue="ui_automation.step_definitions",
-        tags= {"@FETCH-455", "not @wip"},
+        tags= {"@FETCH-545", "not @wip"},
         dryRun = false,
         monochrome = true
 )
